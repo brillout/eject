5dc718c61895d6a34a4afa4a877f3a90d17026cc
Romuald Brillout <git@brillout.com>
2022-11-17 15:10:44 +0100

    fix: improve Stem package resolver

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,11 @@
 import path from 'path'
-import { import_ } from '@brillout/import'
-import { assert, assertUsage, isScriptFile, runCommand, toPosixPath } from './utils'
+import { assert, isScriptFile, runCommand, toPosixPath } from './utils'
 import { projectInfo } from './utils/projectInfo'
 import fse from 'fs-extra'
 import fs from 'fs'
+import { getStemPackages, type StemPackage } from './stemUtils'
+
+const userRootDir = process.cwd()
 
 main()
 
@@ -43,7 +45,8 @@ type Ejectable = {
 }
 
 async function main() {
-  const ejectables = await findEjectables()
+  const stemPackages = await getStemPackages(userRootDir)
+  const ejectables = await findEjectables(stemPackages)
 
   const { stemPackageName, ejectName } = getCliArgs()
   if (!stemPackageName) {
@@ -108,16 +111,16 @@ async function getUserFiles(): Promise<string[]> {
 }
 
 function removeStemPackage(stemPackageName: string) {
-  const pkgJson = getUserPackageJson()
+  const pkgJsonPath = path.join(userRootDir, './package.json')
+  const pkgJson = require(pkgJsonPath)
   assert(Object.keys(pkgJson.dependencies!).includes(stemPackageName))
-  const { pkgPath } = getUserPackageJsonPath()
-  let fileContent = String(fs.readFileSync(pkgPath))
+  let fileContent = String(fs.readFileSync(pkgJsonPath))
   // Hacky but needed if we want to perserve formating
   fileContent = fileContent
     .split('\n')
     .filter((line) => !line.includes(`"${stemPackageName}": "`))
     .join('\n')
-  fs.writeFileSync(pkgPath, fileContent)
+  fs.writeFileSync(pkgJsonPath, fileContent)
 }
 
 function findMatch(ejectables: Ejectable[], stemPackageName: string, ejectName: string | null): null | Ejectable {
@@ -152,87 +155,20 @@ function getCliArgs(): { stemPackageName: null | string; ejectName: null | strin
   return { stemPackageName, ejectName }
 }
 
-async function findEjectables(): Promise<Ejectable[]> {
-  const pkgJson = getUserPackageJson()
-  const stemPackages = getStemPackages(pkgJson)
+async function findEjectables(stemPackages: StemPackage[]): Promise<Ejectable[]> {
   const ejectables = (await Promise.all(stemPackages.map(getEjectablesFromConfig))).flat()
   return ejectables
 }
 
-async function getEjectablesFromConfig(stemPackageName: string): Promise<Ejectable[]> {
-  assert(stemPackageName.startsWith('@') && stemPackageName.includes('stem-'))
-
-  if (stemPackageName === '@brillout/stem-react') {
-    const ejectConfig = (await import_('/home/romuuu/code/stem-react/dist/eject.config.js')).default as EjectConfig
-    const { ejectables } = ejectConfig
-    assert(ejectables.length === 1)
-    const { actions } = ejectables[0]
-    assert(actions.length > 1)
-    return [
-      {
-        stemPackageName,
-        stemPackageRootDir: '/home/romuuu/code/stem-react',
-        ejectName: null,
-        actions
-      }
-    ]
-  }
-
-  const ejectConfigPath = `${stemPackageName}/eject.config.js`
-  let mod: Record<string, unknown>
-  try {
-    mod = await import_(ejectConfigPath)
-  } catch (err) {
-    if ((err as any).code === 'ERR_MODULE_NOT_FOUND') {
-      assertUsage(false, `Couldn't find ${ejectConfigPath}`)
-    }
-    throw err
-  }
-  const stemPackageRootDir = await getStemPackageRootDir(stemPackageName)
-  const ejectConfig: EjectConfig = mod.default as EjectConfig
-  console.log(ejectConfig)
+async function getEjectablesFromConfig(stemPackage: StemPackage): Promise<Ejectable[]> {
+  const { stemPackageName, stemPackageRootDir, loadModule } = stemPackage
+  const moduleExports = await loadModule('eject.config.js')
+  // TODO: assert `ejectConfig`
+  const ejectConfig: EjectConfig = moduleExports.default as EjectConfig
   return ejectConfig.ejectables.map(({ actions, name }) => ({
     stemPackageName,
     stemPackageRootDir,
     actions,
     ejectName: name ?? null
   }))
-}
-
-async function getStemPackageRootDir(stemPackageName: string): Promise<string> {
-  const pkgJsonPath = `${stemPackageName}/package.json`
-  let pathResolved: string
-  try {
-    pathResolved = await import_(pkgJsonPath)
-  } catch {
-    assertUsage(false, `Make sure to (properly) set \`${pkgJsonPath}#exports['package.json']\``)
-  }
-  const stemPackageRootDir = path.posix.dirname(toPosixPath(pathResolved))
-  return stemPackageRootDir
-}
-
-function getStemPackages(pkgJson: UserPackageJson) {
-  const stemPackages = Object.keys(pkgJson?.dependencies ?? {}).filter((depName) =>
-    depName.split('/')[1]?.startsWith('stem-')
-  )
-  return stemPackages
-}
-
-type UserPackageJson = { dependencies?: Record<string, string> }
-
-function getUserPackageJson(): UserPackageJson {
-  const { cwd, pkgPath } = getUserPackageJsonPath()
-  let pkgJson: UserPackageJson
-  try {
-    pkgJson = require(pkgPath)
-  } catch {
-    throw new Error(`No package.json found at ${cwd}`)
-  }
-  return pkgJson
-}
-
-function getUserPackageJsonPath(): { pkgPath: string; cwd: string } {
-  const cwd = process.cwd()
-  const pkgPath = `${cwd}/package.json`
-  return { cwd, pkgPath }
 }
